5eeafee57dbd8d8173b4b8229166a45fd0b24b77
unknown <wnsvy223@naver.com>
2022-10-22 18:32:32 +0900

    서비스워커 알림 백그라운드 메시지 핸들러에 타이틀설정 함수 누락부분 추가

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -23,6 +23,23 @@ firebase.initializeApp({
 });
 const messaging = firebase.messaging();
 
+/**
+ * FCM 페이로드 데이터에서 notificationType, title 값을 받아 타입별로 알림 메시지를 생성하여 반환
+ * @param {알림 타입} notificationType
+ * @param {알림 제목} title
+ * @returns string : 타입별 알림 메시지
+ */
+// eslint-disable-next-line consistent-return
+const setToastTitle = (notificationType, title) => {
+  switch (notificationType) {
+    case 'comment':
+      return `${title}님이 게시글에 댓글을 달았습니다`;
+    case 'childrencomment':
+      return `${title}님이 댓글에 답글을 달았습니다`;
+    default:
+  }
+};
+
 messaging.onBackgroundMessage((payload) => {
   // 백그라운드 푸시 클릭
   self.onnotificationclick = (event) => {
